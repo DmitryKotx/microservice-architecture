c8021e8be12393a7ce8e969f8bb4d473cb31fa40
DimaKotovProgrammist <dskotov003@gmail.com>
2023-07-27 13:02:56 +0300

    minor change

--- customer/src/main/java/ru/kotov/customer/CustomerApplication.java
+++ customer/src/main/java/ru/kotov/customer/CustomerApplication.java
@@ -1,11 +1,8 @@
-package ru.kotov;
+package ru.kotov.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients(
