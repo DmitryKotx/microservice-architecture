90b5df7cdcba946f60608ba2315ac3fc592af75b
DimaKotovProgrammist <dskotov003@gmail.com>
2023-07-27 13:00:20 +0300

    added amqp module and dependencies with configuration for message queue

--- notification/src/main/java/ru/kotov/notification/NotificationApplication.java
+++ notification/src/main/java/ru/kotov/notification/NotificationApplication.java
@@ -1,13 +1,32 @@
 package ru.kotov.notification;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ru.kotov.amqp.RabbitMqMessageProducer;
 
 
-@SpringBootApplication()
+@SpringBootApplication(
+        scanBasePackages = {
+            "ru.kotov.notification",
+            "ru.kotov.amqp",
+})
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+    @Bean
+    CommandLineRunner commandLineRunner(RabbitMqMessageProducer producer, NotificationConfig config) {
+        return args -> {
+            producer.publish(
+                    new Person("Dima", 20),
+                    config.getInternalExchange(),
+                    config.getInternalNotificationRoutingKey()
+            );
+        };
+    }
+     record Person(String name, Integer age){
 
+    }
 }
